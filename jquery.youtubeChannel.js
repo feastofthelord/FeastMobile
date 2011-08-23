d6b6def9d6b1af6ca1ee874f8cb0396af7bf4e92
Miguel Guerreiro <miguel.guerreiro@gmail.com>
2011-08-23 16:57:01 +0100

    Added link to user's channel on top; began extending to allow keyword queries

--- jquery.youtubeChannel.js
+++ jquery.youtubeChannel.js
@@ -1,49 +1,62 @@
 
-(function(){
+;(function(){
 	$.fn.youtubeChannel = function(settings) {
-		var version = '0.1',
-			$ytEl = $(this),
-			$ytList = $('<ul/>',{class:'yt-channel-list'});
-			options = $.extend({}, {username:'',startIndex:1,maxResults:10}, settings);
-		var dataUrl = 'https://gdata.youtube.com/feeds/api/videos?author='+options.username+'&orderby=published&alt=json&start-index='+options.startIndex+'&max-results='+options.maxResults;
-		var videos = [];
-		// accessory functions
-		var parseTime = function(secs) {
-			var m,s = parseInt(secs,10);
-			m = Math.floor(s / 60);
-			s -= (m * 60);
-			return m+':'+s;
-		};
+		var version	= {major:0,minor:3,build:0},
+			$ytEl	= $(this),
+			$ytList	= $('<ul/>',{class:'yt-channel-list'});
+			options	= $.extend({}, {username:'',query:'',startIndex:1,maxResults:10}, settings),
+			videos	= [],
+			// accessory functions
+			getVersion	= function() {
+				return [version.major,version.minor,version.build].join('.');
+			},
+			buildUrl	= function() {
+				var base	= 'https://gdata.youtube.com/feeds/api/videos',
+					params	= ['alt=json','orderby=published'];
+				if (options.username!=='') {
+					params.push('author='+options.username);
+				}
+				else if (options.query!=='') {
+					params.push('q='+encodeURIComponent(options.query));
+				}
+				else return base;
+				params.push('start-index='+options.startIndex);
+				params.push('max-results='+options.maxResults);
+				return base+'?'+params.join('&');
+			},
+			parseTime	= function(secs) {
+				var m,s = parseInt(secs,10);
+				m = Math.floor(s / 60);
+				s -= (m * 60);
+				return m+':'+s;
+			};
 		// setup the html
 		$ytEl.addClass('yt-channel-holder');
 		$ytList.appendTo($ytEl);
 		// parse the feed
-		$.getJSON(dataUrl,function(data) {
+		$.getJSON(buildUrl(),function(data) {
 			// add the header
-			$('<li/>',{class:'yt-channel-title'}).html(options.username).appendTo($ytList);
+			$('<li/>',{class:'yt-channel-title'}).html(['<a href="http://www.youtube.com/user/',options.username,'" target="_blank">',options.username,'</a>'].join('')).appendTo($ytList);
 			// add the items
-			for (var i=data.feed.entry.length-1; i>=0; i--) {
-				var e = data.feed.entry[i],
-					h = {},
-					vid = {
-						title:		e.media$group.media$title.$t,
-						views:		e.yt$statistics.viewCount,
-						duration:	parseTime(e.media$group.yt$duration.seconds),
-						thumb:		e.media$group.media$thumbnail[1].url,
-						link:		e.media$group.media$player[0].url,
-						published:	new Date(Date.parse(e.published.$t))
-					};
-				h.li = $('<li/>',{class:'yt-channel-video'});
-				h.a = $('<a target="_blank" href="'+vid.link+'"/>');
-				h.thumb = $('<span/>',{class:'thumb-wrap'}).html('<img class="vid-thumb" alt="" src="'+vid.thumb+'"/><span class="vid-duration">'+vid.duration+'</span>');
-				h.div = $('<div/>',{class:'vid-details'}).html('<span class="vid-title">'+vid.title+'</span><span class="vid-views">'+vid.views+' views</span>');
-				h.thumb.appendTo(h.a);
-				h.div.appendTo(h.a);
-				h.a.appendTo(h.li);
-				h.li.appendTo($ytList);
+			for (var i=0; i<data.feed.entry.length; i++) {
+				var html,e = data.feed.entry[i];
+				html = $('<li/>',{class:'yt-channel-video'})
+						.html([
+							'<a target="_blank" href="',e.media$group.media$player[0].url,'">',
+								'<span class="thumb-wrap">',
+									'<img class="vid-thumb" alt="',e.media$group.media$title.$t,'" src="',e.media$group.media$thumbnail[1].url,'"/>',
+									'<span class="vid-duration">',parseTime(e.media$group.yt$duration.seconds),'</span>',
+								'</span>',
+								'<div class="vid-details">',
+									'<span class="vid-title">',e.media$group.media$title.$t,'</span>',
+									'<span class="vid-views">',e.yt$statistics.viewCount,' views</span>',
+								'</div>',
+							'</a>'
+						].join(''));
+				html.appendTo($ytList);
 			}
 			// finally add the copyright notice
-			$('<li/>',{class:'yt-channel-copy'}).html('v'+version+'&copy; dharyk 2011').appendTo($ytList);
+			$('<li/>',{class:'yt-channel-copy'}).html('v'+getVersion()+' &copy; dharyk 2011').appendTo($ytList);
 		});
 		return this;
 	};
