73d7cddc769b4aabe1dc09fd388bc93309d50776
Miguel Guerreiro <miguel.guerreiro@gmail.com>
2013-04-03 15:50:31 +0100

    Now returning "api" along with the jQuery object
    The api object is now returned along it the jQuery object, so it can be accessed by other means than just from the callback function;
    Re-factored some code to pass JS Lint (www.jslint.com)
    Added comment with author, license, etc.
    
    Signed-off-by: Miguel Guerreiro <miguel.guerreiro@gmail.com>

--- jquery.youtubeChannel.js
+++ jquery.youtubeChannel.js
@@ -1,24 +1,55 @@
-
-;(function($,undef){
+/**
+ * jQuery.youtubeChannel
+ * version 0.7.1
+ * @requires jQuery >= 1.4
+ * 
+ *   jQuery plugin to retrieve and parse the feed for a youtube
+ *   user's channel, displaying a list of thumbnails with title
+ *   and number of views which link to the video itself
+ * 
+ * 
+ * Author: Miguel Guerreiro (miguel.guerreiro@gmail.com)
+ * Source: http://github.com/dharyk/jQuery.youtubeChannel
+ *
+ * Copyright (c) 2012-2013 Miguel Guerreiro
+ * Licensed under the GPL license:
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ */
+;(function($, undef) {
     $.fn.youtubeChannel = function(settings) {
+        var self = this,
             // this plugin's version
-        var version         = {major:0,minor:5,build:10},
+            version         = {
+                major: 0,
+                minor: 7,
+                build: 1
+            },
             // the API version to use
             apiVersion      = 2,
             // the element where the plugin was called
             $ytEl           = $(this),
             // the list element
-            $ytList         = $('<ul/>',{'class':'yt-channel-list'}),
+            $ytList         = $('<ul/>', {
+                'class': 'yt-channel-list'
+            }),
             // the header
-            $ytHead,
+            $ytHead = undef,
             // the footer (copyright note)
-            $ytFoot,
+            $ytFoot = undef,
             // maximum number of results that the api can return per call
             maxApiResults   = 50,
             // string to hold the html to be inserted
             listHtml        = '',
             // the plugin's options
-            options         = $.extend({}, {username:'',query:'',startIndex:1,maxResults:50,orderBy:'published',callback:function(){}}, settings),
+            options         = $.extend({}, {
+                username: '',
+                query: '',
+                startIndex: 1,
+                maxResults: 50,
+                orderBy: 'published',
+                callback: function() {}
+            }, settings),
             // the current offset (must start at 1)
             resultOffset    = options.startIndex    = (options.startIndex < 1 ? 1 : options.startIndex),
             /*  -- API OBJECT --  */
@@ -26,26 +57,38 @@
                 // failed to get videos?
                 failed: false,
                 // the array of videos
-                videos: [],
+                videos: {},
                 // number of videos
                 videoCount: 0
             },
             /*  -- PLUGIN FUNCTIONS --  */
             // get the html for the header
             getTitle    = function() {
-                if (options.username!=='') {
-                    return ['<a href="http://www.youtube.com/user/',options.username,'" target="_blank">',options.username,'</a>'].join('');
+                var out;
+                if (options.username !== '') {
+                    out = [
+                        '<a href="http://www.youtube.com/user/',
+                        options.username,
+                        '" target="_blank">',
+                        options.username,
+                        '</a>'
+                    ].join('');
+                } else if (options.query !== '') {
+                    out = [
+                        '<a href="http://www.youtube.com/results?',
+                        encodeURIComponent(options.query),
+                        '&aq=f" target="_blank">&quot;',
+                        options.query,
+                        '&quot;</a>'
+                    ].join('');
+                } else {
+                    out = '<a href="http://www.youtube.com/" target="_blank">YouTube</a>';
                 }
-                else if (options.query!=='') {
-                    return ['<a href="http://www.youtube.com/results?',encodeURIComponent(options.query),'&aq=f" target="_blank">&quot;',options.query,'&quot;</a>'].join('');
-                }
-                else {
-                    return '<a href="http://www.youtube.com/" target="_blank">YouTube</a>';
-                }
+                return out;
             },
             // get the plugin's version
             getVersion  = function() {
-                return [version.major,version.minor,version.build].join('.');
+                return [version.major, version.minor, version.build].join('.');
             },
             // calculate the next set of results to request from the API
             nextSet     = function() {
@@ -55,52 +98,65 @@
             // build the url to make the API call
             buildUrl    = function() {
                 var base    = 'https://gdata.youtube.com/feeds/api/videos',
-                    params  = ['alt=json','v='+apiVersion,'orderby='+options.orderBy,'start-index='+resultOffset,'max-results='+nextSet()];
-                if (options.username!=='') {
-                    params.push('author='+options.username);
-                }
-                else if (options.query!=='') {
-                    params.push('q='+encodeURIComponent(options.query));
+                    params  = [
+                        'alt=json',
+                        'v=' + apiVersion,
+                        'orderby=' + options.orderBy,
+                        'start-index=' + resultOffset,
+                        'max-results=' + nextSet()
+                    ];
+                if (options.username !== '') {
+                    params.push('author=' + options.username);
+                } else if (options.query !== '') {
+                    params.push('q=' + encodeURIComponent(options.query));
                 }
                 params.push('callback=?');
-                return base+'?'+params.join('&');
+                return base + '?' + params.join('&');
             },
             // parse the videos' time (from secs to mins:secs)
             parseTime   = function(secs) {
-                var m,s = parseInt(secs,10);
+                var m, s = parseInt(secs, 10);
                 m = Math.floor(s / 60);
                 s -= (m * 60);
-                return m+':'+s;
+                return [m, s].join(':');
             },
             // add a video to the list
             addVideo    = function(vid) {
                 // change the id to be more html friendly
-                vid.htmlId = 'videoid-'+vid.id;
+                vid.htmlId = 'videoid-' + vid.id;
                 // add video data to the videos array
-                api.videos.push(vid);
+                api.videos[vid.id] = vid;
                 // return the styled HTML
                 return [
-                    '<li id="',vid.htmlId,'" class="yt-channel-video">',
-                        '<a target="_blank" href="',vid.link,'">',
-                            '<span class="thumb-wrap">',
-                                '<img class="vid-thumb" alt="',vid.title,'" src="',vid.thumb,'"/>',
-                                '<span class="vid-duration">',parseTime(vid.duration),'</span>',
-                            '</span>',
-                            '<div class="vid-details">',
-                                '<span class="vid-title">',vid.title,'</span>',
-                                '<span class="vid-views">',vid.views,' views</span>',
-                            '</div>',
-                        '</a>',
-                    '</li>'].join('');
+                    '<li id="', vid.htmlId, '" class="yt-channel-video">',
+                    '<a target="_blank" href="', vid.link, '">',
+                    '<span class="thumb-wrap">',
+                    '<img class="vid-thumb" alt="', vid.title, '" src="', vid.thumb, '"/>',
+                    '<span class="vid-duration">', parseTime(vid.duration), '</span>',
+                    '</span>',
+                    '<div class="vid-details">',
+                    '<span class="vid-title">', vid.title, '</span>',
+                    '<span class="vid-views">', vid.views, ' views</span>',
+                    '</div>',
+                    '</a>',
+                    '</li>'
+                ].join('');
+            },
+            // output the final HTML
+            outputHtml  = function() {
+                // append the list of videos
+                $ytFoot.before(listHtml);
+                // clear the html string for further loadMore calls
+                listHtml = '';
             },
             // parse the list of videos sent from the API
             parseList   = function(data) {
-                var vid, e, feedlen;
+                var e, i, feedlen;
                 // do we have videos to add?
                 if (data.feed.entry) {
                     feedlen = data.feed.entry.length;
                     // parse each video returned
-                    for (var i=0; i<feedlen; i++) {
+                    for (i = 0; i < feedlen; i++) {
                         // local cache of the video entry
                         e = data.feed.entry[i];
                         // add the video to the videos array and return the HTML for the list
@@ -118,7 +174,7 @@
                     // check if we want to list more results
                     if (resultOffset < options.maxResults) {
                         // make one more api call for more results
-                        $.getJSON(buildUrl(),parseList);
+                        $.getJSON(buildUrl(), parseList);
                     } else {
                         // done retrieving videos, compile the HTML
                         outputHtml();
@@ -136,33 +192,31 @@
                     // use callback, if set
                     options.callback.apply(self, [api]);
                 }
-            },
-            // output the final HTML
-            outputHtml  = function() {
-                // append the list of videos
-                $ytFoot.before(listHtml);
-                // clear the html string for further loadMore calls
-                listHtml = '';
             };
         /*  -- API FUNCTIONS --  */
         api.loadMore = function loadMore(num) {
             // increase the maximum number of results
             options.maxResults += parseInt(num, 10);
             // make one more api call for more results
-            $.getJSON(buildUrl(),parseList);
+            $.getJSON(buildUrl(), parseList);
         };
         /*  -- PLUGIN MAIN CODE --  */
         // apply styling to the parent element
         $ytEl.addClass('yt-channel-holder');
         // set the header and append it
-        $ytHead = $('<li/>',{'class':'yt-channel-title'}).html(getTitle()).appendTo($ytList);
+        $ytHead = $('<li/>', {
+            'class': 'yt-channel-title'
+        }).html(getTitle()).appendTo($ytList);
         // set copyright notice and append it
-        $ytFoot = $('<li/>',{'class':'yt-channel-copy'}).html('v'+getVersion()+' &copy; dharyk 2011').appendTo($ytList);
+        $ytFoot = $('<li/>', {
+            'class': 'yt-channel-copy'
+        }).html('v' + getVersion() + ' &copy; dharyk 2011').appendTo($ytList);
         // display the list of videos
         $ytList.appendTo($ytEl);
         // start querying the API
-        $.getJSON(buildUrl(),parseList);
+        $.getJSON(buildUrl(), parseList);
         // maintain jQuery chainability
+        self.api = api;
         return this;
     };
-})(jQuery);
+}(jQuery));
